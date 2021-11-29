0b0a79d343de33ad4d4b42296d48be0dd8060c05
Beto Pereira <contato@betopereira.com>
2021-11-29 20:08:12 -0300

    Add the first class algorithm

---
+++ algorithm-class-1.js
@@ -0,0 +1,38 @@
+/*
+Algorithm square of an number
+
+integer: q, n
+
+start
+  write("Type a number: ")
+  read n
+  q ← n*n
+  write("The squre is " + q)
+end
+*/
+
+let q, n;
+n =  prompt("Type a number");
+q = n * n
+console.log(`The square is ${q}`)
+
+/*
+Algorithm average value
+
+real: media, a, b
+
+start
+  write("Type the first number: ")
+  read a
+  write("Type the second number: ")
+  read b
+  media ← (a + b) / 2
+  write("The media value is " + media)
+end
+*/
+
+let a, b, m
+a = prompt("Type the first number")
+b = prompt("Type the second number")
+
+m = (a + b) / 2
