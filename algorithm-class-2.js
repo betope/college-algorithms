ed5d12318e9742ba85a7fe1c3e51984b244bf974
Beto Pereira <contato@betopereira.com>
2021-11-30 18:44:29 -0300

    Solved more two exercises

---
+++ algorithm-class-2.js
@@ -0,0 +1,15 @@
+//Exercise 1 - Get the name and price of two medicines and show to the user
+let med1, med2, sellPrice1, sellPrice2
+med1 = prompt("Type the name of the first medicine")
+sellPrice1 = prompt("Type the price of the first medicine")
+med2 = prompt("Type the name of the second medicine")
+sellPrice2 = prompt("Type the price of the second medicine")
+console.log(`The first medicine name is ${med1} and its price is ${sellPrice1}`)
+console.log("The second medicine name is " + med2 + " and its price is " + sellPrice2)
+
+// Exercise 8
+let employee, empPosition, dependents
+employee = prompt("What's the name of the employee?")
+empPosition = prompt("What's the employee position?")
+dependents = prompt("How many dependents does the employee have?")
+console.log(`Employee name: ${employee} \n Employee position: ${empPosition} \n Employee dependents: ${dependents}`)
