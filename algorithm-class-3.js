0d7fd42344f6d13943dcfa243ea4cb6cded4e574
Beto Pereira <contato@betopereira.com>
2021-12-29 20:21:10 -0300

    Solved exercise 3

--- algorithm-class-3.js
+++ algorithm-class-3.js
@@ -21,3 +21,22 @@ function calcArea(area, sideA, sideB) {
 	console.log(`The rectangle area is: ${area}`)
 }
 calcArea();
+
+// Exercise 3
+let purchaseValue, numInstallments, finalPay;
+purchaseValue = Number(prompt("Type the how much did you pay"));
+numInstallments = Number(prompt("How many installments are you going to pay?"));
+
+function calcPurchase() {
+	finalPay = 5 * purchaseValue / 100 + purchaseValue;
+	let instValue = finalPay / numInstallments;
+	console.log(`You'll pay a total of: ${finalPay}, in ${numInstallments} installments each one with the value of: ${instValue}`)
+}
+calcPurchase();
+// Exercise 4
+// Exercise 5
+// Exercise 6
+// Exercise 7
+// Exercise 8
+// Exercise 9
+// Exercise 10
