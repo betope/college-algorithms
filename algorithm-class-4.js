16acf5671985cd747059a8bef93151278cc84230
Beto Pereira <contato@betopereira.com>
2022-01-09 18:00:45 -0300

    Solved exercise 2

--- algorithm-class-4.js
+++ algorithm-class-4.js
@@ -9,3 +9,20 @@ function verify(num) {
   }
 }
 verify()
+
+// Exercise 2
+function getInfo() {
+	shift = prompt("Type N for night and M for morning");
+  workedHours = Number(prompt("Type how many hours the employee worked today"));
+}
+function calcSalary(salary) {
+	getInfo();
+	if(shift == 'N' || 'n') {
+  	salary = workedHours * 45;
+    console.log(`The employee salary is: ${salary}`);
+  } else {
+  	salary = workedHours * 37.50
+  	console.log(`The employee salary is: ${salary}`);
+  }
+}
+calcSalary()
