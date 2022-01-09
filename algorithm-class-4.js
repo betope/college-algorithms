362bd5f29a8c93ad64bd4f245510b0113c1669ca
Beto Pereira <contato@betopereira.com>
2022-01-09 17:46:28 -0300

    Solved exercise 1

--- algorithm-class-4.js
+++ algorithm-class-4.js
@@ -1,1 +1,11 @@
 // Exercise 1
+function verify(num) {
+	num = Number(prompt("Type a number"));
+
+  if(num >= 0 && num <= 9) {
+  	console.log("Correct value");
+  } else {
+  	console.log("Incorrect value")
+  }
+}
+verify()
